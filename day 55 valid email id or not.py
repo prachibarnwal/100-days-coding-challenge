a53ec40083ce487d570748cd3ee2ef7c7223989e
Prachiiiiii <97833293+prachibarnwal@users.noreply.github.com>
2022-10-07 22:35:10 +0530

    Add files via upload

---
+++ day 55 valid email id or not.py
@@ -0,0 +1,21 @@
+def isValidEmail(email):
+    if email.endswith(".com"):
+        if email[0] != '@' and email.count("@") == 1:
+            for ch in email:
+                if ch.isalpha() or ch.isdigit() or ch == "." or ch == "_" or ch == "@":
+                    continue
+                else:
+                    return False
+            else:
+                return True
+        else:
+            return False
+    else:
+        return False
+
+
+email = input("Enter Email-id : ")
+if isValidEmail(email):
+    print("Invalid E-mail id")
+else:
+    print("Invalid E-mail id")
