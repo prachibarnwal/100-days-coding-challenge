1fa95eb90c89d61417eb67eee0e7e0f752e303b2
Prachiiiiii <97833293+prachibarnwal@users.noreply.github.com>
2023-12-29 22:49:42 +0530

    Add files via upload

---
+++ day 92 operator overloading.py
@@ -0,0 +1,49 @@
+# add is for +
+# sub is for -
+# mul is for *
+# truediv for /
+# floordiv for //
+
+class Point:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return "(x is {0}, y is {1})".format(self.x, self.y)
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Point(x, y)
+    def __mul__(self, other):
+        x = self.x * other.x
+        y = self.y * other.y
+        return Point(x, y)
+    def __sub__(self, other):
+        x = self.x - other.x
+        y = self.y - other.y
+        return Point(x, y)
+    def __mod__(self, other):
+        x = self.x % other.x
+        y = self.y % other.y
+        return Point(x, y)
+    def __truediv__(self, other):
+        x = self.x / other.x
+        y = self.y / other.y
+    def __floordiv__(self, other):
+        x = self.x // other.x
+        y = self.y // other.y
+    
+
+p1 = Point(5,7)
+p2 = Point(2,4)
+
+print("First Point is : ", p1)
+print("Second Point is : ", p2)
+
+print("Remainder : ", p1 % p2)
+print("Sum : ", p1 + p2)
+print("Difference : ", p1 - p2)
+print("Product : ", p1 * p2)
+print("Floor Division : ", p1 // p2)
+print("Quotient : ", p1 / p2)
