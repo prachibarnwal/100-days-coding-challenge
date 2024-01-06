b2804d26becd5f341d4e328760f85cacf7f82d5d
Prachiiiiii <97833293+prachibarnwal@users.noreply.github.com>
2024-01-06 21:41:22 +0530

    Add files via upload

--- day 98 multiple bargraphs.py
+++ day 98 multiple bargraphs.py
@@ -2,15 +2,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-c1 = [1900, 1000, 1400, 1800]
-c2 = [1600, 2200, 1500, 1600]
-c3 = [1900, 2500, 1300, 1800]
+c1 = [1200, 1600, 2590, 1800,2050]
+c2 = [1300, 1700, 2400, 1900, 2100]
+c3 = [1450, 1800, 2590, 2000, 2150]
 
 
-years = ['2018','2019','2020','2021']
+years = ['2018','2019','2020','2021','2022']
 df = pd.DataFrame({"Years" : years, "Company A" : c1, "Company B" : c2, "Company C" : c3})
 df.index = df['Years']
-df.plot(kind = 'bar', color = ['magenta', 'pink', 'yellow', 'orange', 'pink'])
+df.plot(kind = 'bar', color = ['yellow', 'orange', 'red'])
 plt.title("Sales Record 2018-2021")
 plt.xlabel("Sale Year")
 plt.ylabel("Sales in Lakhs INR")
