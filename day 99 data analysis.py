388f3d702d051e0c9e9f5f7892f387a9893a560e
Prachiiiiii <97833293+prachibarnwal@users.noreply.github.com>
2024-01-07 21:37:04 +0530

    Add files via upload

--- day 99 data analysis.py
+++ day 99 data analysis.py
@@ -1,16 +1,19 @@
 #creating series
 import pandas as pd
 
-stu = {"Rohit" : 90, "Kanika" : 88, "Ruhana" : 96, "Shobit" : 65, "Ziyan" : 99}
+stu = {"Rohit" : 70, "Kanika" : 88, "Ruhana" : 96, "Shobit" : 65, "Ziyan" : 79}
 
 s = pd.Series(stu)
 print(s)
-print("*"*40)
-print(s.size)
-print(s.nbytes)
-print("*"*40)
+print("*"*45)
+print("Size of Series : ",s.size)
+print("Number of Bytes Taken : ",s.nbytes)
+print("*"*45)
+print("Students Having Marks >= 80 : ")
 print(s[s >= 80])
-print("*"*40)
+print("*"*45)
+print("Marks After Deducing 2 From Each : ")
 print(s-2)
-print("*"*40)
+print("*"*45)
+print("Data in Sorted Order of Marks : ")
 print(s.sort_values())
