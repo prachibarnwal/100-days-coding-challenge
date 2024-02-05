a6cf1bb7bef8d4f6d5b5fec3f0e364887418ceae
Prachiiiiii <97833293+prachibarnwal@users.noreply.github.com>
2024-02-05 21:00:18 +0530

    Add files via upload

--- day 92 operator overloading.py
+++ day 92 operator overloading.py
@@ -9,7 +9,7 @@ class Point:
         self.x = x
         self.y = y
     def __str__(self):
-        return "(x is {0}, y is {1})".format(self.x, self.y)
+        return "({0}, {1})".format(self.x, self.y)
 
     def __add__(self, other):
         x = self.x + other.x
@@ -30,20 +30,24 @@ class Point:
     def __truediv__(self, other):
         x = self.x / other.x
         y = self.y / other.y
+        return Point(x,y)
     def __floordiv__(self, other):
         x = self.x // other.x
         y = self.y // other.y
-    
+        return Point(x,y)
+
 
 p1 = Point(5,7)
 p2 = Point(2,4)
 
+print("*"*32)
 print("First Point is : ", p1)
 print("Second Point is : ", p2)
-
+print("*"*32)
 print("Remainder : ", p1 % p2)
 print("Sum : ", p1 + p2)
 print("Difference : ", p1 - p2)
 print("Product : ", p1 * p2)
 print("Floor Division : ", p1 // p2)
 print("Quotient : ", p1 / p2)
+print("*"*32)
